7f7af979c07376858ad16d69b9446e414e8f232f
Sarjak <bhandarisarjak@gmail.com>
2024-04-28 14:11:59 +0545

    if else statement

--- Week2/Task1.java
+++ Week2/Task1.java
@@ -4,9 +4,7 @@ public class Task1 {
     public static void main(String[] args) {
             //WAP to print whether a variable is greater or equal to 18
     int a =10;
-    boolean b = (a>=18);
+    String b = (a>=18) ? "Greater!!!" : "Not greater";
     System.out.println(b);
-    }
-
-    
+    }    
 }
