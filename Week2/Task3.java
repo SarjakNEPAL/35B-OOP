91ecc760e7c0a1ecec4fbe64786207652982a255
Sarjak <bhandarisarjak@gmail.com>
2024-05-03 20:06:47 +0545

    Task 5

--- Week2/Task3.java
+++ Week2/Task3.java
@@ -6,12 +6,17 @@ public class Task3 {
          * area = length * breadth
          * perimeter = 2 (length + breadth)
         */
-    double length, breadth ,area, perimeter;
+    double length, breadth ,area, perimeter,volCuboid,volCube,height;
     length = 1020.55d;
     breadth= 1234.22d;
+    height=1234.22d;
     area = length * breadth;
     perimeter = 2*(length + breadth);
+    volCuboid = length*breadth*height;
+    volCube= length*length*length;
     System.out.println("area = "+area);
     System.out.println("perimeter="+perimeter);
+    System.out.println("volume of cuboid="+volCuboid);
+    System.out.println("volume of cube-"+volCube);
     }
 }
