6fd6db8a660a8bc840cf507e360158ebecd21366
Sarjak <bhandarisarjak@gmail.com>
2024-05-09 15:28:01 +0545

    Static function java continue

--- Week5/StaticFunction.java
+++ Week5/StaticFunction.java
@@ -5,11 +5,27 @@ public class StaticFunction {
         int multiplication =a*b;
         return multiplication;
     }
+    public static String vendingMachine(int menu){
+        if (menu==1) {
+            return "Coke";
+        }
+        else if (menu == 3) {
+            return "water";
+        }
+        else if (menu==3) {
+            return "Furti";            
+        }
+        else{
+            return "Invalid Input";
+        }
+        }
     public static void main(String[] args) {
         /*if the funtion is static and in same class
          * no need to make object simply call
          */
         int multiplyReturn = multiply(10,2);
         System.out.println("Return is "+multiplyReturn);
+        String iGet=vendingMachine(1);
+        System.out.println("I get "+iGet);
     }
 }
