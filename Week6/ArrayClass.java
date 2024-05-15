2739b4ab8e9c5d58933c3f96458b990a85f78ba9
Sarjak <bhandarisarjak@gmail.com>
2024-05-15 14:49:10 +0545

    notes of the class

---
+++ Week6/ArrayClass.java
@@ -0,0 +1,25 @@
+package Week6;
+
+public class ArrayClass {
+    public static void main(String[] args) {
+        //array declaration
+        int[] myFirstArray;
+        //memory allocation
+        myFirstArray = new int[3];
+        //array declaratom and memory alloc*
+        int[] mySecondArray = new int[3];
+        //array declaration and memory allocation and initialozatoin
+        int[] myThirdArray={10,20,30,40};
+
+        //Array index ->position of element and values
+        //starts with 0
+        mySecondArray[0]=10;
+        mySecondArray[1]=20;
+        mySecondArray[2]=100;
+
+        //access/store in new variable
+        int arrayElement=mySecondArray[0];
+        System.out.println("First index"+arrayElement);
+        System.out.println("Second index" + mySecondArray[1]);
+    }
+}
