6d152711980e1b81d0ad21ecd74a80aaf3b6f819
Sarjak <bhandarisarjak@gmail.com>
2024-05-15 15:27:28 +0545

    task

---
+++ Week6/ArrayLoop.java
@@ -0,0 +1,38 @@
+package Week6;
+
+import static org.junit.jupiter.api.DynamicTest.stream;
+
+public class ArrayLoop {
+    public static void main(String[] args) {
+        int[] arr =new int[4];
+        arr[0]= -10;
+        arr[1]=0;   
+        arr[2]=10;
+        arr[3]=20;
+        //we need length of array first, uuse .length
+        int arrayLength= arr.length;
+        //loop from 0 - (arrayLength -1), 0-3
+        for(int index=0; index <arrayLength;index++){
+            System.out.println(arr[index]);
+        }
+        
+        // or we can se condition index <=arrayLength-1
+        System.out.println("Decending Loop");
+        for(int index= arrayLength -1 ;index >=0; index--){
+            System.out.println(arr[index]);
+        }
+
+        //lopp the  arr using while loop
+        //make an array of ["John","Doe","Foo","Bar"]
+        //Make a string "Foo"
+        //Loop the array and find in which index is "Foo" on
+        //print the index
+        String[] words={"John","Doe","Foo","Bar"};
+        String istiring="Foo";
+        for(int i=0; i<words.length;i++){
+            if(words[i]==istiring){
+                System.out.println(i);
+            }
+        }
+    }
+}
